5881ac764567e18cd564e92a3e544f982571217c
valentyn <Valentun_Prodyser@ukr.net>
2016-07-10 14:07:46 +0300

    Added configuration of Hibernate and JPA.

---
+++ src/main/java/com/familytime/config/PersistenceContex.java
@@ -0,0 +1,64 @@
+package com.familytime.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+/**
+ * Created by valentyn on 7/10/16.
+ */
+
+@Configuration
+@EnableJpaRepositories("com.familytime.model.repository")
+@EnableTransactionManagement
+@PropertySource("classpath:config/DB/config.properties")
+public class PersistenceContex {
+
+    @Autowired
+    private Environment env;
+    
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("db.driver"));
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername( env.getProperty("db.username") );
+        dataSource.setPassword( env.getProperty("db.password"));
+        return dataSource;
+    }
+    
+    @Bean
+    public EntityManagerFactory entityManagerFactory() {
+        
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("com.familytime");
+        factory.setDataSource(dataSource());
+        factory.afterPropertiesSet();
+
+        return factory.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory());
+        return txManager;
+    }
+}
